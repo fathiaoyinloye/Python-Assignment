e09fe6fdef0e9ca1991cb71964a93c8776c79607
fathiaoyinloye <you@example.com>
2025-09-29 09:27:41 +0100

    pie

---
+++ Chapter Three Python Assignment/pie.py
@@ -0,0 +1,12 @@
+pie = 4;
+count = 1
+denominator = 3
+numerator = 4
+while(count <= 10):
+	pie = pie + numerator/ denominator
+	denominator = denominator + 2
+	count = count + 1
+
+print(f"{pie: .2f}")
+
+
